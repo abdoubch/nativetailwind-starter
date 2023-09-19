94d6432227a509a98b530415b325a3337377c7f5
abdoubch <la_bouchendouka@esi.dz>
2023-09-20 00:08:19 +0200

    first commit

--- App.js
+++ App.js
@@ -3,8 +3,8 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View className="flex-1 justify-center items-center bg-black">
+      <Text className="text-white">Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
   );
